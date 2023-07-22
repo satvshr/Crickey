455bb3bc48c5c082a8e001273ab28f86bff74444
satvshr <112589278+satvshr@users.noreply.github.com>
2023-07-22 19:04:07 +0530

    Final bot made

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
-import discord, csv
+import os, discord, csv
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env")
 
 def read_csv_file(csv_file_path):
     with open(csv_file_path, 'r') as file:
@@ -10,9 +13,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-bot_token = 'MTEzMTIxMzc4NDcwMzMwMzc2MA.GKNGkb.oRvN45_wFRrXay02w8MuRZrM2YbCxZ3_oWasoo'
-
-is_On = False
+bot_token = os.getenv('BOT_TOKEN')
 
 @client.event
 async def on_ready():
@@ -21,27 +22,20 @@ async def on_ready():
     print(f'Logged in as {client.user.name}')
 
 @client.event
-async def on_message(message):
-    global is_On
-    if message.content == '!start':
-        is_On = True
-        await message.channel.send('Hello, I am your friendly bot!')
-        
-    elif message.content == '!stop':
-        is_On = False
-        await message.channel.send('Goodbye! Hope I was helpful to you!')
-
-    elif message.content == '!livescore':
-        if is_On:
+async def on_message(message):      
+    if message.content == '!livescore':
             await message.channel.send('Fetching the live scores...')
             all_rows = read_csv_file('liveScores.csv')
             if all_rows == None:
                 await message.channel.send('No live scores available!')
             else:
                 await message.channel.send(f"{all_rows[0]} \n {all_rows[1]} \n {all_rows[2]}")
-        else:
-            await message.channel.send('Please start the bot first!')
 
     elif message.content == '!help':
-        await message.channel.send('Commands: \n !start - start the bot \n !stop - stop the bot \n !livescore - get the live scores')
+        await message.channel.send('Commands: \n !csv - get the csv file the livescores are stored in \n !livescore - get the live scores')
+
+    elif message.content == '!csv':
+        file = discord.File('liveScores.csv')
+        await message.channel.send(file=file)
+
 client.run(bot_token)
